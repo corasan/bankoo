40908bddc0151abfa68901d1b221ec2a9f64f90e
henrypl95 <henrypl360@gmail.com>
2015-08-23 12:47:40 -0400

    generated the scaffold for transactions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822213426) do
+ActiveRecord::Schema.define(version: 20150823163721) do
+
+  create_table "transactions", force: :cascade do |t|
+    t.date     "date"
+    t.float    "amount"
+    t.string   "type"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
