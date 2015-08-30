e823879252bc9082da7fc68a1ec185b28511a3ef
henrypl95 <henrypl360@gmail.com>
2015-08-30 12:57:06 -0400

    removed some columns from users and transactions table

--- db/migrate/20150823213230_add_user_balance_to_users.rb
+++ db/migrate/20150823213230_add_user_balance_to_users.rb
@@ -1,5 +1,5 @@
 class AddUserBalanceToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :user_balance, :float, default: 0.00
-  end
+    def change
+        add_column :users, :user_balance, :float, default: 0.00
+    end
 end
